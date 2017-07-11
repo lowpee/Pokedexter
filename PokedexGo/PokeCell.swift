66271729e4b5064cb22d4a82f69ff954fcb3dc7a
Matt <mattlopez.j@gmail.com>
2017-07-11 00:10:14 -0700

    pushing project to gitHub

---
+++ PokedexGo/PokeCell.swift
@@ -0,0 +1,33 @@
+//
+//  PokeCell.swift
+//  PokedexGo
+//
+//  Created by Lord Lopie on 6/29/17.
+//  Copyright © 2017 DivLabs. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+    
+}
